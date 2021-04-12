e52af17631c73cb9d90e34c49e94533776353a3f
Duncan Kolba <duncankolba@gmail.com>
2021-04-12 18:21:53 +0200

    Add proper canonical link in head

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,24 @@
 import Head from "next/head";
 
 function PodcastApp({ Component, pageProps }) {
-  const storyTitle = process.env.STORY_TITLE;
-  const crawling = process.env.CRAWLING;
+  const { statusCode } = pageProps;
+  if (statusCode === "404") {
+    return null;
+  }
+  const {
+    slug,
+    configuration: { protocol, domain, storyTitle, crawling },
+  } = pageProps;
+  const canonical = slug
+    ? `${protocol}://${domain}/${slug}`
+    : `${protocol}://${domain}`;
+
   return (
     <>
       <Head>
         <title>{storyTitle}</title>
         <meta name="robots" content={crawling} />
+        <link href={canonical} rel="canonical" />
       </Head>
       <Component {...pageProps} />
     </>
